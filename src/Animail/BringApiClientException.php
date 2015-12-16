ef9a151235aa384af97190a009e3b1ca7db780f4
Joakim Hedlund <contact@joakimhedlund.com>
2015-12-16 10:42:32 +0100

    Namespaces are tricky!

--- src/Animail/BringApiClientException.php
+++ src/Animail/BringApiClientException.php
@@ -3,7 +3,7 @@ namespace Animail;
 
 class BringApiClientException extends BringApiException
 {
-  public function __construct($message = null, $code = 0, Exception $previous = null)
+  public function __construct($message = null, $code = 0, \Exception $previous = null)
   {
     // make sure everything is assigned properly
     parent::__construct($message, $code, $previous);
--- src/Animail/BringApiException.php
+++ src/Animail/BringApiException.php
@@ -1,9 +1,9 @@
 <?php
 namespace Animail;
 
-class BringApiException extends Exception
+class BringApiException extends \Exception
 {
-  public function __construct($message = null, $code = 0, Exception $previous = null)
+  public function __construct($message = null, $code = 0, \Exception $previous = null)
   {
     // make sure everything is assigned properly
     parent::__construct($message, $code, $previous);
